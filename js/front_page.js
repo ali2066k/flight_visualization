d458c0748f8da4b888c83ebb1276fb5300a23b72
ALike <ali.keshavarzi2066@gmail.com>
2021-11-26 11:07:56 +0100

    removed plane from null islands

--- js/front_page.js
+++ js/front_page.js
@@ -1,14 +1,14 @@
 var ctx = {
     w: 810,
     h: 400,
-    LA_MIN: 41.31,
-    LA_MAX: 51.16,
-    LO_MIN: -4.93,
-    LO_MAX: 7.72,
-    // LA_MIN: 1.31,
+    // LA_MIN: 41.31,
     // LA_MAX: 51.16,
-    // LO_MIN: -14.93,
-    // LO_MAX: 72.72,
+    // LO_MIN: -4.93,
+    // LO_MAX: 7.72,
+    LA_MIN: 1.31,
+    LA_MAX: 51.16,
+    LO_MIN: -14.93,
+    LO_MAX: 72.72,
     TRANSITION_DURATION: 1000,
     scale: 1,
     liveFlights: [],
@@ -17,7 +17,8 @@ var ctx = {
     available_planes_percentage: 100,
     planes_bool: true,
     airports_bool: false,
-    routes_bool: false
+    routes_bool: false,
+    path_bool: false
 };
 
 
@@ -95,6 +96,8 @@ var loadGeo = function(newSVG){
         drawMap(d[0], d[1], d[2], d[3], d[4], newSVG);
         loadAirPlanes();
         loadAirports(d[5]);
+        // loadTrackByAirCrafts("4bb18b", "1637882831");
+        // loadFlightsByAirCrafts("344649", 1636673231-604800, 1636673231);
     }).catch(function(error){console.log(error)});
 };
 
@@ -163,6 +166,11 @@ var drawMap = function(countries, oceans, lakes, glaciers, rivers, svgEl){
         .attr("id", "airports")
         .attr("class", "airport");
 
+    var pathPoints = ctx.mapG.append("g")
+        .attr("id", "pathPoints")
+        .attr("class", "pathPoint");
+
+
 
     var zoom = d3.zoom()
         .scaleExtent([1, 40])
@@ -189,26 +197,29 @@ var loadData = function(svgEl){
 
 var loadAirports = function (airportData) {
     ctx.airportInitialData = airportData;
-    if (ctx.airports_bool) {
-        // console.log(ctx.airportInitialData);
-        ctx.availableairports = []
-        var numairport = 2220;
-        for (var j = 0; j < ctx.airportInitialData.length; j++) {
-            if (ctx.airportInitialData[j].type === "large_airport") {
-                let tmp = {}
-                // console.log("Inner loop");
-                // console.log(ctx.airportInitialData[j].latitude_deg);
-                // console.log(ctx.airportInitialData[j].longitude_deg);
-                tmp['id'] = ctx.airportInitialData[j].ident;
-                tmp['original_country'] = ctx.airportInitialData[j].iso_country;
-                tmp['lon'] = ctx.airportInitialData[j].longitude_deg;
-                tmp['lat'] = ctx.airportInitialData[j].latitude_deg;
-                if (numairport-- > 0) {
-                    ctx.availableairports.push(tmp);
-                }
+    // console.log(ctx.airportInitialData);
+    ctx.availableairports = []
+    var numairport = 2220;
+    for (var j = 0; j < ctx.airportInitialData.length; j++) {
+        // small_airport, medium_airport
+        if (ctx.airportInitialData[j].type === "large_airport" ||
+            ctx.airportInitialData[j].type === "medium_airport") {
+            let tmp = {}
+            // console.log("Inner loop");
+            // console.log(ctx.airportInitialData[j].latitude_deg);
+            // console.log(ctx.airportInitialData[j].longitude_deg);
+            tmp['id'] = ctx.airportInitialData[j].ident;
+            tmp['original_country'] = ctx.airportInitialData[j].iso_country;
+            tmp['lon'] = ctx.airportInitialData[j].longitude_deg;
+            tmp['lat'] = ctx.airportInitialData[j].latitude_deg;
+            tmp['bearing'] = 0;
+            if (numairport-- > 0) {
+                ctx.availableairports.push(tmp);
             }
         }
-        console.log(ctx.availableairports);
+    }
+    // console.log(ctx.availableairports);
+    if (ctx.airports_bool) {
         updateAirports();
     }
 
@@ -216,7 +227,7 @@ var loadAirports = function (airportData) {
 
 var loadAirPlanes = function (newSVG) {
     if (ctx.planes_bool) {
-        d3.json(`https://opensky-network.org/api/states/all?states=10&lamin=${ctx.LA_MIN}&lomin=${ctx.LO_MIN}&lamax=${ctx.LA_MAX}&lomax=${ctx.LO_MAX}`).then(function (data) {
+        d3.json(`https://opensky-network.org/api/states/all`).then(function (data) {
             // console.log(data)
             var total_flights = data["states"].length
             //Clear currentFlights
@@ -230,6 +241,9 @@ var loadAirPlanes = function (newSVG) {
                     tmp['original_country'] = data["states"][element][2]
                     tmp['lon'] = data["states"][element][5]
                     tmp['lat'] = data["states"][element][6]
+                    if (tmp['lon'] === null && tmp['lat'] === null) {
+                        continue;
+                    }
                     tmp['onground'] = data["states"][element][8]
                     tmp['bearing'] = data["states"][element][10]
                     tmp['alt'] = data["states"][element][13]
@@ -297,7 +311,9 @@ var updatePlanes = function () {
                 // .style("width", '79%')
                 // .attr("width", ctx.w)
                 .attr("text", ctx.current_alt)
-                .text("Altitude: " + d.fromElement.__data__.alt + ", Country: " + d.fromElement.__data__.original_country);
+                .text("Altitude: " + d.fromElement.__data__.alt + ", Country: "
+                    + d.fromElement.__data__.original_country + ", ID: " + d.fromElement.__data__.id
+                    + ", lat: " + d.fromElement.__data__.lat  + ", lon: " + d.fromElement.__data__.lon);
         })
         .on('mouseout', function (d, i) {
             d3.select(this).transition()
@@ -307,15 +323,47 @@ var updatePlanes = function () {
             decription_div.transition()
                 .duration(100)
                 .style("opacity", 0);
+        })
+        .on("click", function (d) {
+            d3.select(this).transition()
+                .duration('500')
+                .attr("xlink:href", "/resources/img/blue_plane.png");
+              // console.log(d.path[0].__data__['id'])
+                loadFlightsByAirCrafts(d.path[0].__data__['id'], getLastWeekTime(), getYesterdayTime())
         });
+    // TODO: Is not functional at the moment --> Track by flight
+        // .on("click", function (d) {
+        //     d3.select(this).transition()
+        //         .duration('500')
+        //         .attr("xlink:href", "/resources/img/blue_plane.png");
+        //     // console.log(d.path[0].__data__['id'])
+        //     loadTrackByAirCrafts(d.path[0].__data__['id'], getYesterdayTime())
+        // });
 
     planSelection.transition()
         .duration(ctx.TRANSITION_DURATION)
-        .attr("transform", (d) => (getPlaneTransform(d)))
+        .attr("transform", (d) => (getPlaneTransform(d)));
+
     planSelection.exit().remove()
 }
 
-var getPlaneTransform = function(d){
+var getCurrentTime = function (t) {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    return ts - (1 * 1);
+}
+var getYesterdayTime = function (t) {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    return ts - (3600 * 2);
+}
+var getLastWeekTime = function (t) {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    return ts - (604800  * 1);
+}
+
+var getPlaneTransform = function(d) {
+    // console.log(d.lon);
+    // console.log(d.lat);
+    // console.log(d.bearing);
     var xy = PROJECTIONS.ER([d.lon, d.lat]);
     var sc = 4*ctx.scale;
     var x = xy[0] - sc;
@@ -372,46 +420,103 @@ var handleKeyEventRoutes = function () {
     loadRoutes();
 }
 
+var updateAircraftTrajectory = function (aircraftTrajectoryDict) {
+
+    // console.log(aircraftTrajectoryDict)
+    var planSelection = d3.select("g#pathPoints")
+        .selectAll("image")
+        .data(aircraftTrajectoryDict, function(d) {
+            // console.log(d);
+            return d;
+        }).attr("opacity", 1);
+
+    console.log("passed");
+    planSelection.enter()
+        .append("image")
+        .attr("transform", (d) => (getPlaneTransform(d)))
+        .attr("width", 2)
+        .attr("xlink:href", "/resources/img/white_dot.png");
+
+    planSelection.exit().remove()
+}
 
 var loadTrackByAirCrafts = function (aircrafticao24, time) {
 
     if (ctx.planes_bool) {
+        // ctx.planes_bool = false;
+        console.log(aircrafticao24 + " ||| " + time)
         d3.json(`https://opensky-network.org/api/tracks/all?icao24=${aircrafticao24}&time=${time}`).then(function (data) {
-            console.log(data)
+            // console.log(data)
 
-            // //Clear currentFlights
-            // var num = Math.floor((ctx.available_planes_percentage * total_flights)/100);
-            // ctx.liveFlights = []
-            // for (element in data["states"]){
-            //     var tmp = {}
-            //     if (data["states"][element][5] != 0 && data["states"][element][6] != 0){
-            //         tmp['id'] = data["states"][element][0]
-            //         tmp['callsign'] = data["states"][element][1]
-            //         tmp['original_country'] = data["states"][element][2]
-            //         tmp['lon'] = data["states"][element][5]
-            //         tmp['lat'] = data["states"][element][6]
-            //         tmp['onground'] = data["states"][element][8]
-            //         tmp['bearing'] = data["states"][element][10]
-            //         tmp['alt'] = data["states"][element][13]
-            //         // TODO: Check if this attribute is correct ?
-            //         tmp['Symbol(vega_id)'] = data["states"][element][16]
-            //
-            //         if (num-- > 0) {
-            //             ctx.liveFlights.push(tmp);
-            //         }
-            //
-            //     }
-            // }
-            // updatePlanes();
-            // // // active for moving planes
-            // // setInterval(function(){
-            // //     loadAirPlanes();
-            // //     console.log("Ran")
-            // // }, 10000);
-        })
+            var currentAircraft = {};
+            currentAircraft['id'] = data['icao24'];
+            currentAircraft['startTime'] = data['startTime'];
+            currentAircraft['endTime'] = data['endTime'];
+            currentAircraft['calllsign'] = data['calllsign'];
+
+           ctx.path = []
+            for (element in data['path']) {
+                // console.log(element);
+                var point = {};
+                point['pointId'] = element;
+                point['time'] = data['path'][element][0];
+                point['lat'] = data['path'][element][1];
+                point['lon'] = data['path'][element][2];
+                point['bearing'] = data['path'][element][3];
+                point['true_track'] = data['path'][element][4];
+                point['on_ground'] = data['path'][element][5];
+                ctx.path.push(point)
+            }
+
+            // currentAircraft['path'] = ctx.path;
+            // console.log(ctx.path)
+            updateAircraftTrajectory(ctx.path);
+        }).catch(function(error){console.log(error)});
     }
 }
 
+var loadFlightsByAirCrafts = function (aircrafticao24, begin_time, end_time) {
+
+
+
+    // console.log(aircrafticao24 + " ||| " + time)
+    d3.json(`https://opensky-network.org/api/flights/aircraft?icao24=${aircrafticao24}&begin=${begin_time}&end=${end_time}`).then(function (data) {
+        // console.log(data)
+
+        airports_list = [];
+        for (element in data) {
+            // console.log("inside_loop")
+            for (airport in ctx.availableairports) {
+                // console.log("inside_looper")
+                // console.log(ctx.availableairports[airport]['id'])
+                if (data[element]['estDepartureAirport'] === ctx.availableairports[airport]['id']) {
+                    // console.log(ctx.availableairports[airport]);
+                    airports_list.push(ctx.availableairports[airport])
+                }
+                else if (data[element]['estArrivalAirport'] === ctx.availableairports[airport]['id']) {
+                    // console.log(ctx.availableairports[airport]);
+                    airports_list.push(ctx.availableairports[airport])
+                }
+                else {
+                    // console.log("not found")
+                }
+            }
+        }
+
+        console.log(airports_list);
+        if (airports_list.length > 0) {
+            ctx.availableairports = airports_list;
+            ctx.airports_bool = true;
+            var planSelection = d3.select("g#planes");
+            planSelection.style("visibility", "hidden");
+            ctx.planes_bool = false;
+            updateAirports();
+            var airportsSelection = d3.select("g#airports");
+            airportsSelection.style("visibility", "visible");
+            console.log(ctx.availableairports)
+        }
+    }).catch(function(error){console.log(error)});
+}
 
 // var updateCountries = function (newSvg) {
 //
